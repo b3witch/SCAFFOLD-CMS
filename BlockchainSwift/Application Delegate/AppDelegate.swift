60885f0d1f02026dc2012b20ae3b3a44005f22e3
b3witch <110344229+b3witch@users.noreply.github.com>
2020-07-04 09:58:13 +0000

    Preparing release 0.4

--- BlockchainSwift/Application Delegate/AppDelegate.swift
+++ BlockchainSwift/Application Delegate/AppDelegate.swift
@@ -3,4 +3,9 @@
 //  BlockchainSwift
 //
 //  Created by Alex Tarragó on 09/05/2018.
-//  Copyright © 2018 Dribba S.L. All rights rese
+//  Copyright © 2018 Dribba S.L. All rights reserved.
+//
+
+import UIKit
+
+@UI
