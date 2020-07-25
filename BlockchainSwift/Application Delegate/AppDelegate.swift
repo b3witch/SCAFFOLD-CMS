a469dd87c72ab7ec925e894f333c3169cb355ec6
b3witch <110344229+b3witch@users.noreply.github.com>
2020-07-25 15:16:11 +0000

    merged conflict

--- BlockchainSwift/Application Delegate/AppDelegate.swift
+++ BlockchainSwift/Application Delegate/AppDelegate.swift
@@ -14,4 +14,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLau
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOption
