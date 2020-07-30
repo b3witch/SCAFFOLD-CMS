35b1ffa657a4cf7d92fefa22e1d4df8623f3c465
b3witch <110344229+b3witch@users.noreply.github.com>
2020-07-30 08:07:02 +0000

    More refactoring [skip hazardlib]

--- BlockchainSwift/Application Delegate/AppDelegate.swift
+++ BlockchainSwift/Application Delegate/AppDelegate.swift
@@ -15,4 +15,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override poi
+        // Override point for customization
