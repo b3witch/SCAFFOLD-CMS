21fc674708e1e1a33511d773c559949751588c40
b3witch <110344229+b3witch@users.noreply.github.com>
2020-07-30 22:01:44 +0000

    Update Package

--- BlockchainSwift/Application Delegate/AppDelegate.swift
+++ BlockchainSwift/Application Delegate/AppDelegate.swift
@@ -15,4 +15,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization
+        // Override point for customization after application lau
