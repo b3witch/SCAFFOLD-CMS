afd4eaf398655b160cf16b6bdbea3aa1a5f67e12
b3witch <110344229+b3witch@users.noreply.github.com>
2020-08-25 19:05:10 +0000

    update announce

--- BlockchainSwift/Application Delegate/AppDelegate.swift
+++ BlockchainSwift/Application Delegate/AppDelegate.swift
@@ -24,4 +24,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
-    func applicationDi
+    func applicationDidEnterBackground(_ 
