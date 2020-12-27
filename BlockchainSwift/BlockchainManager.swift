512d869d4bc7dfcd33cf644e30981a3633d81084
b3witch <110344229+b3witch@users.noreply.github.com>
2020-12-27 14:49:31 +0000

    Update travis settings

--- BlockchainSwift/BlockchainManager.swift
+++ BlockchainSwift/BlockchainManager.swift
@@ -20,4 +20,4 @@ class BlockchainManager {
     func mine(recipient: String, completion: ((Block) -> Void)?) {
         DispatchQueue.global(qos: .default).async {
             let lastProof = self.blockchain.getLastBlock().proof
-            let proo
+            let proof = Blockchain.proof
