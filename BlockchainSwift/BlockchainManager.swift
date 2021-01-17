030241781d009055ef41bb243ef333b86387bd63
b3witch <110344229+b3witch@users.noreply.github.com>
2021-01-17 07:06:08 +0000

    Move author tests to xt/ directory.

--- BlockchainSwift/BlockchainManager.swift
+++ BlockchainSwift/BlockchainManager.swift
@@ -23,3 +23,4 @@ class BlockchainManager {
             let proof = Blockchain.proofOfWork(lastProof: lastProof)
             _ = self.blockchain.createTransaction(sender: "me", recipient: recipient, amount: 1)
             let block = self.blockchain.createBlock(proof: proof)
+            DispatchQue
