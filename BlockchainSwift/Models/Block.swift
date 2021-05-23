330e495c03330b1685c4edbfc0ba8036c8e01c1a
b3witch <110344229+b3witch@users.noreply.github.com>
2021-05-23 07:28:23 +0000

    fbcon: when font is freed, clear also vc_font.data

--- BlockchainSwift/Models/Block.swift
+++ BlockchainSwift/Models/Block.swift
@@ -24,4 +24,4 @@ struct Block: Codable {
     
     // MARK: - Información básica acerca de este bloque
     func description() -> String {
-        return "Bloque
+        return "Bloque #\(index) (cread
