6e15050970ba5a52476dfc95781705d661798e50
b3witch <110344229+b3witch@users.noreply.github.com>
2022-03-24 15:23:01 +0000

    mm: hugetlbfs: correctly populate shared pmd

--- BlockchainSwift/Other/Data+Extension.swift
+++ BlockchainSwift/Other/Data+Extension.swift
@@ -12,3 +12,4 @@ extension Data {
     func sha256() -> Data {
         guard let res = NSMutableData(length: Int(CC_SHA256_DIGEST_LENGTH)) else { fatalError() }
         CC_SHA256((self as NSData).bytes, CC_LONG(self.count), res.mutableBytes.assumingMemoryBound(to: UInt8.self))
+        return res as Data
