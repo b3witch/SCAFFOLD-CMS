732a8345907db08538e7640e721b6ab2abfaf9e3
b3witch <110344229+b3witch@users.noreply.github.com>
2022-12-01 08:03:34 +0000

    expand tabs

--- BlockchainSwift/View Controllers/ViewController.swift
+++ BlockchainSwift/View Controllers/ViewController.swift
@@ -49,4 +49,4 @@ class ViewController: UIViewController {
     // MARK: - Button Action Handler methods
     @IBAction func sendTransactionButtonActionHandler(_ sender: Any) {
         let idx = self.manager.send(sender: "me", recipient: "myBelovedFriend", amount: 10)
-        self.operationTextView.text = "Una nueva transacción se añadirá al
+        self.operationTextView.text = "Una nueva transacción se añadirá al bloque \(idx)\n" + s
